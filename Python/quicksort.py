2ee9672e4c8ccb5c6c667effea6972785cf4c6e6
Isaac_Santos <isaacdsantos@hotmail.com>
2025-01-01 18:21:54 -0300

    Refatorado quicksort afim de corrigir erro de ordenacao

--- Python/quicksort.py
+++ Python/quicksort.py
@@ -4,47 +4,47 @@
 from palavras import palavras
 import random
 
-def choosepivotrandom(l,r):
+def indexpivot(input_list,l,r):
     # escolha aleatorio de um pivo eh combinacao de escolher
     # entre o primeiro elemento ou outro qualquer proximo da mediana
     return random.randint(l,r)
 
+def partition(input_list,l,r): 
+        i = l
+        j = r
+        pivot = input_list[indexpivot(input_list,l,r)]
+        
+        while True:
+            while input_list[i] < pivot:
+                i+=1
+            while input_list[j] > pivot:
+                j-=1
+            if i <= j:
+                input_list[i], input_list[j] = input_list[j], input_list[i]
+                i+=1
+                j-=1
+            if i > j:
+                break
+        
+        return i,j
+    
+def orders(input_list,l,r): 
+    
+    i,j = partition(input_list,l,r)
+    
+    if l < j:
+        orders(input_list,l,j)
+    if i < r:
+        orders(input_list,i,r)
+            
+
 def quicksort(input_list):
-    
-    def choosepivot(input_list,l,r):
-    
-        return choosepivotrandom(l,r)
 
-    def partition(input_list,l,r):
-        p = input_list[l]
-        i = l + 1
-        aux = 0
-        for j in range(l+1,r)
-            if input_list[j] < p :
-                aux = input_list[j]
-                input_list[j] = input_list[i]
-                input_list[i] = aux
-                i += 1
-            aux = input_list[l]
-            input_list[l] = input_list[i-1]
-            input_list[i-1] = aux
-        
-        return i - 1
+    orders(input_list,0,len(input_list)-1) 
+   
     
-    def orders(input_list,l,r):
-        
-        if l >= r: 
-            return None
-        
-        pivot = choosepivot(input_list,l,r)
-        
-        aux = input_list[pivot]
-        input_list[pivot] = input_list[l]
-        input_list[l] = aux
-
-        new_pivot = partition(input_list,l,r)
-        
-        orders(input_list,l,new_pivot-1)
-        orders(input_list,new_pivot+1,r)
-
-    orders(input_list,0,len(input_list)-1)
+print("\n Palavras em ordem qualquer \n")
+print(palavras)
+quicksort(palavras)
+print("\n Palavras ordenadas pelo algoritmo QuickSort \n")
+print(palavras)
