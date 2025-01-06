46c5c3e2cb0d3b7a59f9758811ac872cabb29631
Isaac_Santos <isaacdsantos@hotmail.com>
2025-01-06 16:04:25 -0300

    Criada a funcao shellsort

---
+++ Python/shellsort.py
@@ -0,0 +1,38 @@
+
+from palavras import palavras
+
+def shellsort(input_list):
+
+    h = 1
+    n = len(input_list)
+    
+    while True:             # repita
+        h = 3*h+1           #   h = 3*h+1    
+        if h>n:             # ateh h > n
+            break
+    
+    while True:                                  # repita
+    
+        
+        h = h % 3
+        
+        for i in range(h+1,n):
+            x = input_list[i]
+            j = i
+             
+            while input_list[j-h] > x:
+               input_list[j] = input_list[j-h]
+               j-=h
+               if j<=h :                        #           if j <= h :
+                   break                        #               goto Label
+        
+            input_list[j] = x
+                                                # Label
+        if h == 1:
+            break
+
+print("\n Palavras em ordem qualquer \n")
+print(palavras)
+shellsort(palavras)
+print("\n Palavras ordenadas pelo algoritmo ShellSort \n")
+print(palavras)
