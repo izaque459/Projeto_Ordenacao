a244ee98f3c82461e26a141e3ac562d42f8eeaff
Isaac_Santos <isaacdsantos@hotmail.com>
2025-01-20 19:48:12 -0300

    Modificado funcao shakersort, o codigo refatorado

--- Python/shakersort.py
+++ Python/shakersort.py
@@ -1,5 +1,5 @@
 from palavras import palavras
-# Data: 06/01/2025
+# Data: 20/01/2025
 # O metodo SkakerSort é uma variação do metodo BubbleSort 
 # Cada iteração ocorre nas dois sentidos do array
 
@@ -7,29 +7,36 @@ def shakersort(input_list):
     n = len(input_list)
     l = 0  # Índice inicial da esquerda
     r = n - 1  # Índice inicial da direita
-    k = 0
 
-    while l < r:
+    while True:
+        
         for j in range(l, r):  # Itera da esquerda para a direita
             if input_list[j] > input_list[j + 1]:
                 input_list[j], input_list[j + 1] = input_list[j + 1], input_list[j]
-                k = j + 1 # k recebe o valor do indice do ultimo swap
-
-        r = k #atualiza o indice da direita com o ultimo indice do swap
-        if l >= r:
-            break
+                k = j  # k recebe o valor do indice do ultimo swap
+                
+        r = k
 
         for j in range(r, l, -1):  # Itera da direita para a esquerda
             if input_list[j] < input_list[j - 1]:
                 input_list[j], input_list[j - 1] = input_list[j - 1], input_list[j]
-                k = j - 1 # k recebe o valor do indice do ultimo swap
+                k = j # k recebe o valor do indice do ultimo swap
                 
-        l = k #atualiza o indice da esquerda com o ultimo indice do swap
+        l = k  #atualiza o indice da esquerda com o ultimo indice do swap
+        
         if l >= r:
             break
+
+teste = sorted(palavras)
+
 
 print("\n Palavras em ordem qualquer \n")
 print(palavras)
 shakersort(palavras)
 print("\n Palavras ordenadas pelo algoritmo ShakerSort \n")
 print(palavras)
+
+if palavras == teste:
+    print(" \nPalavras ordenadas corretamente\n")
+else:
+    print(" \n Palavras ordenadas incorretamente \n")
