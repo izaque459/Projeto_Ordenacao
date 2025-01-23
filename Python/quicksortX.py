b1d23524c9363f24a2efd6603a1b5d7367fddb2a
Isaac_Santos <isaacdsantos@hotmail.com>
2025-01-23 16:28:34 -0300

    REfatoracao de quicksort para quicksortX, refatorando como escolher o indice

---
+++ Python/quicksortX.py
@@ -0,0 +1,81 @@
+#data:01/01/2025
+# o famoso algorimto quicksort eh o algoritmo que uma vez bem 
+# implementado pode se tornar o favorito em ordenacoes
+from palavras import palavras
+import random
+
+def indexpivot(input_list,l,r):
+    # escolha aleatoriamente três elementos da lista
+    # para retornar o indice da mediana desses três
+    
+    #    if (input_list[i] < input_list[j] and input_list[j] < input_list[k]) or (input_list[k] < input_list[j] and input_list[j] < input_list[i]):
+    #        return  j
+    #    elif (input_list[j] < input_list[i] and input_list[i]< input_list[k]) or (input_list[k] < input_list[i] and input_list[i]< input_list[j]):
+    #        return i
+    #    else:
+    #        return k
+
+    i = random.randint(l,r)
+    j = random.randint(l,r)
+    k = random.randint(l,r)
+    
+    a = input_list[i]
+    b = input_list[j]
+    c = input_list[k]
+
+    if (a <= b <= c) or (c <= b <= a):
+        return j
+    elif (b <= a <= c) or (c <= a <= b):
+        return i
+    else:
+        return k
+    
+
+
+def partition(input_list,l,r): 
+        i = l
+        j = r
+        pivot = input_list[indexpivot(input_list,l,r)]
+        
+        while True:
+            while input_list[i] < pivot:
+                i+=1
+            while input_list[j] > pivot:
+                j-=1
+            if i <= j:
+                input_list[i], input_list[j] = input_list[j], input_list[i]
+                i+=1
+                j-=1
+            if i > j:
+                break
+        
+        return i,j
+    
+def orders(input_list,l,r): 
+    
+    i,j = partition(input_list,l,r)
+    
+    if l < j:
+        orders(input_list,l,j)
+    if i < r:
+        orders(input_list,i,r)
+            
+
+def quicksortX(input_list):
+
+    orders(input_list,0,len(input_list)-1) 
+   
+    
+
+teste = sorted(palavras)
+
+print("\n Palavras em ordem qualquer \n")
+print(palavras)
+quicksortX(palavras)
+print("\n Palavras ordenadas pelo algoritmo QuickSort X \n")
+print(palavras)
+
+if palavras == teste:
+    print(" \nPalavras ordenadas corretamente\n")
+else:
+    print(" \n Palavras ordenadas incorretamente \n")
