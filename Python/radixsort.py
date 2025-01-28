f4e647a18978c9b877e7624f032ac3a5e36dd8f6
Isaac_Santos <isaacdsantos@hotmail.com>
2025-01-28 18:57:44 -0300

    Adicionado funcao radixsort

---
+++ Python/radixsort.py
@@ -0,0 +1,58 @@
+from counting import gerarLista
+
+def countingsort(arr, exp):
+    
+    n = len(arr)
+    output = [0] * n
+    count = [0] * 10  # Assume base 10 (0-9)
+
+    for i in range(n):
+        index = (arr[i] // exp) % 10  # Extrai o dígito
+        count[index] += 1
+
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+
+    for i in range(n - 1, -1, -1):
+        index = (arr[i] // exp) % 10
+        output[count[index] - 1] = arr[i]
+        count[index] -= 1
+
+    return output
+
+def radixsort(input_list):
+    
+    m = max(input_list)
+    exp = 1
+       
+    while m / exp >= 1:
+        output = coutingsort(input_list)
+        exp *= 10
+        
+    return output
+    
+
+# Exemplo de uso:
+tam = 25  # Tamanho da lista a ser gerada
+minimo = 0   # Valor mínimo dos números aleatórios
+maximo = 1100 # Valor máximo dos números aleatórios
+
+try:
+    lista_para_ordenar = gerarlista(tam, minimo, maximo)
+
+    print("Lista não ordenada:", lista_para_ordenar)
+
+    lista_ordenada = countingsort(lista_para_ordenar)
+    print("Lista ordenada:", lista_ordenada)
+
+except ValueError as e:
+    print(f"Erro: {e}")
+   
+   
+# Teste de corretude 
+lista_ordenada_python = sorted(lista_para_ordenar)
+if lista_ordenada == lista_ordenada_python:
+    print("\nOrdenação correta!")
+else:
+    print("\nOrdenação INCORRETA!")
+        
